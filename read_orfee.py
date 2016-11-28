5572018d341005e8d4aff7b3a84148ea6336b0cc
Laura Hayes <hayesla@tcd.ie>
2016-11-28 12:19:34 +0000

    plotting added

--- read_orfee.py
+++ read_orfee.py
@@ -43,6 +43,10 @@ TIME_B5 = orfee_image.data['time_b4']/1000.0
 STOKESI_B5 = orfee_image.data['stokesi_b5']
 STOKESV_B5 = orfee_image.data['stokesv_b5']
 
+all_data = np.concatenate((STOKESI_B1.T, STOKESI_B2.T, STOKESI_B3.T, STOKESI_B4.T, STOKESI_B5.T))
+
+all_time = np.concatenate((TIME_B1, TIME_B2, TIME_B3, TIME_B4, TIME_B5))
+
 #function to make time array into datetime array
 def make_datetime(time_ar):
 	time_array = []
@@ -59,3 +63,60 @@ t_b5 = make_datetime(TIME_B5)
 
 
 
+## pandas series of freq (205MHz) that interested in
+
+#pulsations = Series(STOKESI_B1[:,150], index = t_b1).truncate('2014-04-18 12:54', '2014-04-18 12:58') #205mhz
+pulsations1 = Series(STOKESI_B1[:,315], index = t_b1).truncate('2014-04-18 12:54', '2014-04-18 12:58') #270Mhz
+pulsations3 = Series(STOKESI_B1[:,207], index = t_b1).truncate('2014-04-18 12:54', '2014-04-18 12:58')#228 mhz
+pulsations2 = Series(STOKESI_B2[:,15], index = t_b2).truncate('2014-04-18 12:54', '2014-04-18 12:58') #327mhz
+
+###Fourier analysis function##
+def fourier_ana(x,dt):
+	N = len(x)
+	dt = dt
+	df = 1./(N*dt)
+	PSD = abs(dt*fftpack.fft(x)[:N/2])**2
+	f = df*np.arange(N/2)
+	plt.plot(1./f, PSD)
+	plt.xlim(0,100)
+	A = np.where(PSD == max(PSD))[0][0]
+	return f, PSD
+
+
+#plotting
+import seaborn as sns
+sns.set_style('ticks',{'xtick.direction':'in','ytick.direction':'in'})
+sns.set_context('paper')
+
+import matplotlib as mpl
+label_size = 15
+mpl.rcParams['xtick.labelsize'] = label_size 
+mpl.rcParams['ytick.labelsize'] = label_size 
+
+plt.figure(figsize=(15, 10))
+test = STOKESI_B1[...,100:315] #slicing columns from 186 - 270 MHz
+test = test[16401:17001] #truncating from 12:56:20 - 12:57:20
+test_freq = freq_b1[100:315]
+test_time = t_b1[16401:17001]
+
+
+xx = [0,100,200,300,400,500]
+xticks = []
+for i in xx:
+	xticks.append(str(test_time[i])[11:19])
+#l = np.locgical_and(test_freq > 200, test_freq <201)
+#np.where(l == True)
+yy = [36, 87, 137, 189]
+yticks = test_freq[yy]
+for i in range(len(yticks)):
+	yticks[i] = str(yticks[i])[0:3]
+
+cmap = 'magma_r'
+
+plt.imshow(test.T, aspect = 'auto', cmap = cmap)
+plt.xticks(xx, xticks)
+plt.yticks(yy, yticks)
+plt.title('Orfee Spectrogram', fontsize = 20)
+plt.ylabel('Frequencies (MHz)', fontsize = 20)
+plt.xlabel('Start time ' + str(test_time[0])[0:19] + ' UT', fontsize = 20)
+plt.tight_layout()
